1b309e773f87c6c0a3da30cf72a79788b52d4df9
dananjaya6005 <dananjaya6005@gmail.com>
2023-06-21 11:35:06 +0530

    render image and detils using map fucntion

--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ root.render(
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-    
   </React.StrictMode>
 );
 
--- src/pages/CharacterList.js
+++ src/pages/CharacterList.js
@@ -1,29 +1,55 @@
 import React from 'react'
-import { useQuery,gql } from '@apollo/client'
+import { useQuery, gql } from '@apollo/client'
 
 export default function CharacterList() {
 
-    const GET_CHARACHTERS = gql`
+  const GET_CHARACTERS = gql`
     query {
-        characters{
-          results{
-            name
-            gender
-            image
-            
-          }
+      characters {
+        results {
+          name
+          gender
+          image
         }
       }
-      
-    `;
+    }
+  `;
 
-    const {error,loading,data} = useQuery(GET_CHARACHTERS);
+  const { error, loading, data } = useQuery(GET_CHARACTERS);
 
-    console.log({error,loading,data});
-
+  console.log({ error, loading, data });
 
   return (
-    <div>CharacterList</div>
+    <div>
+      {data && data.characters && data.characters.results.map((character) => {
+        return (
+          <div key={character.name}>
+            <h2>{character.name}</h2>
+            <img src={character.image} alt={character.name} />
+          </div>
     
-  )
+        );
+
+      })
+      
+      }
+
+  {/* {data && data.characters ? 
+    data.characters.results.map((character) => {
+      return (
+        <div key={character.name}>
+          <h2>{character.name}</h2>
+          <img src={character.image} alt={character.name} />
+        </div>
+      );
+    })
+    : null
+  } */}
+
+
+
+
+
+    </div>
+  );
 }
