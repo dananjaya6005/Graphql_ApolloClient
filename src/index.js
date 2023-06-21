b8103b832f296f7a4b427afd5de764c5511f10f3
dananjaya6005 <dananjaya6005@gmail.com>
2023-06-21 10:49:24 +0530

    Import apollo client

--- src/index.js
+++ src/index.js
@@ -3,17 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ApolloProvider,InMemoryCache,ApolloClient } from '@apollo/client';
 
 //https://rickandmortyapi.com/graphql
+
+const client = new  ApolloClient({
+  uri : "https://rickandmortyapi.com/graphql",
+  cache : new InMemoryCache()
+})
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <ApolloProvider>
       <App />
+    </ApolloProvider>
     
   </React.StrictMode>
 );
